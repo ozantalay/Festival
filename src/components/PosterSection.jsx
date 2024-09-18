053233c86b4262078066da0d8e25ec96f9e56fa9
ozantalay <ozntalay@hotmail.com>
2024-09-18 14:10:04 +0300

    final

--- src/components/PosterSection.jsx
+++ src/components/PosterSection.jsx
@@ -1,10 +1,11 @@
+// import { group } from 'console'
 import { useState } from 'react'
 
 export default function PosterSection({ date, festivalData }) {
   /*Challenge
 	
 	Müzikseverlerin bu yılki Festival'de hangi gün hangi grupların çalacağını bilmesi gerekiyor. Sizin göreviniz bu PosterSection'ı aşağıdaki gibi bu işi yapabilecek hale getirmek: 
-	
+
 		1. Aşağıdaki kodlanmış paragraflar, yinelemeli olarak oluşturulan paragraflarla değiştirilmelidir - festivalData array'indeki her bir grup için, bileşene bir prop olarak aktarılan günde (1, 2 veya 3 Haziran) çalan bir grup. Belirli bir günün bölümünde görünmesi gereken *sadece* gruplar o gün çalan gruplardan oluşmalıdır.
 		   
 		2. Gruplar önem sırasına göre sıralanmalıdır (festivalData array'indeki nesnelerin "importance" özelliği). 1'den 4'e kadar sıralanırlar, 1 en önemli ve 4 en az önemli olandır. İki grup aynı öneme sahipse, array'de daha önce görünen PosterBölümü'nde ilk olarak görünmelidir. 
@@ -13,24 +14,28 @@ export default function PosterSection({ date, festivalData }) {
 		
 		4. Bu görevleri doğru bir şekilde tamamlarsanız, 1 Haziran bölümünde yalnızca 1 Haziran 'da çalan grupları görmelisiniz; en önemlileri üstte büyük puntolarla, daha az önemlileri ise altta daha küçük puntolarla yer almalıdır. Aynı şey 2 ve 3 Haziran için de geçerlidir. Sayfanın altında otomatik olarak bir footer da görünmelidir. 
 
+		 */
 
+    const sortedGroups = festivalData.filter(group=>group.date===date)
+    .sort((a,b)=>a.importance-b.importance)
 
-		   
- */
+    const getClassName = (importance) => {
+      if (importance === 1) return 'giant';
+      if (importance === 2) return 'big';
+      if (importance === 3) return 'medium';
+      return 'small';
+    };
 
   return (
     <div className='lineup-container'>
       <div className='day-container'>
         <h3>{date}</h3>
       </div>
+      {sortedGroups.map((group, index) => (
+        <p key={index} className={getClassName(group.importance)}>{group.name} </p>
+    ))}
+    
 
-      <p className='giant'>Süper Büyük Önemli Grup</p>
-      <p className='giant'>Başka Biri</p>
-      <p className='big'>Biraz Daha Az Önemli Grup</p>
-      <p className='big'>Başka Biri</p>
-      <p className='medium'>Orta Dereceli Grup</p>
-      <p className='medium'>Başka Biri</p>
-      <p className='small'>O kadar da önemli olmayan bir grup</p>
     </div>
   )
 }
